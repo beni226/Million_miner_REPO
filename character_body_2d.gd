c0267488ca1025ac1071fca49ca841a72a2a3397
beni226 <benjamin.joseph8797@gmail.com>
2025-08-14 12:05:00 -0400

    movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -200.0
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +13,13 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("ui_down") and is_on_floor():
+		$AnimatedSprite2D.play("crouch")
+	else:
+		$AnimatedSprite2D.play("idle")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
