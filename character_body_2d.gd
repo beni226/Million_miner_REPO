f3204be338241062125372eeb1b3c485bd771d16
betdot <richemondkaeshon1234@gmail.com>
2025-09-05 12:07:54 -0400

    dig fixes

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -44,7 +44,8 @@ func _physics_process(delta: float) -> void:
 		dig2()
 	if Input.is_action_just_pressed("digR"):
 		dig3()
-	
+	if Input.is_action_just_pressed("digU"):
+		dig4()
 
 	move_and_slide()
 	#Digging action script
@@ -57,12 +58,18 @@ func dig():
 func dig2():
 	var tilemap = get_parent().get_node("TileMap") # Adjust path to your TileMap
 	var local_pos = tilemap.to_local(global_position)
-	var cell = tilemap.local_to_map(local_pos + Vector2(-27, -15))
+	var cell = tilemap.local_to_map(local_pos + Vector2(15, -45))
 	tilemap.set_cell(0, cell, -1) # Layer 0, removes the tile
 	tilemap.set_cell(1,cell,-1)	
 func dig3():
 	var tilemap = get_parent().get_node("TileMap") # Adjust path to your TileMap
 	var local_pos = tilemap.to_local(global_position)
-	var cell = tilemap.local_to_map(local_pos + Vector2(47, -27)) # Offset to dig below player
+	var cell = tilemap.local_to_map(local_pos + Vector2(47, -45)) # Offset to dig below player
 	tilemap.set_cell(0, cell, -1) # Layer 0, removes the tile
 	tilemap.set_cell(1,cell,-1)	
+func dig4():
+	var tilemap = get_parent().get_node("TileMap") # Adjust path to your TileMap
+	var local_pos = tilemap.to_local(global_position)
+	var cell = tilemap.local_to_map(local_pos + Vector2(25,-65)) # Offset to dig below player
+	tilemap.set_cell(0, cell, -1) # Layer 0, removes the tile
+	tilemap.set_cell(1,cell,-1)
