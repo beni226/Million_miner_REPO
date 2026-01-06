adb7c20e8f17831742d5d2a937e72ace4691b021
beni226 <benjamin.joseph8797@gmail.com>
2026-01-06 13:19:16 -0500

    working on deaty zone

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -85,3 +85,28 @@ func dig4():
 	var cell = tilemap.local_to_map(local_pos + Vector2(25,-65)) # Offset to dig below player
 	tilemap.set_cell(0, cell, -1) # Layer 0, removes the tile
 	tilemap.set_cell(1,cell,-1)
+	
+	
+	
+	
+#Death function
+func die():
+	print("Player has Died!")
+
+	# Move player to respawn point
+	global_position = Vector2(100, 100) # Change to your respawn point
+
+	# Reset movement
+	velocity = Vector2.ZERO
+	jump_count = 0
+
+	# Temporarily disable collision to avoid instantly re-triggering death zone
+	set_deferred("collision_layer", 0)
+	set_deferred("collision_mask", 0)
+	
+	# Restore collisions next frame
+	call_deferred("_restore_collision")
+
+func _restore_collision():
+	collision_layer = 1
+	collision_mask = 1
