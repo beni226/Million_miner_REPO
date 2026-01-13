5c3c48bab3abf69513f3d7dda038d32f164a48cd
beni226 <benjamin.joseph8797@gmail.com>
2026-01-13 11:40:00 -0500

    bra

--- killPlane.gd
+++ killPlane.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
+@export var kill_delay := 5.0          # Optional delay before death
+@export var killzone_size := Vector2(64, 64)
 
-@export var kill_delay := 0.0   # optional delay before death
+@onready var collision := $CollisionShape2D
+
+func _ready():
+	# Create a unique collision shape for this instance
+	var shape := RectangleShape2D.new()
+	shape.size = killzone_size
+	collision.shape = shape
+'''	
+func kill_delay_timer(kill_delay):
+	while kill_delay 
+'''
 
 func _on_body_entered(body):
 	if body.is_in_group("players"):
