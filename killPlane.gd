445091369c2bda86bb829686e719e6ad2b43a224
beni226 <benjamin.joseph8797@gmail.com>
2026-01-13 12:05:47 -0500

    working1

--- killPlane.gd
+++ killPlane.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 
-@export var kill_delay := 0.0   # optional delay before death
+@export var kill_delay := 1.0   # optional delay before death
+@export var damage_amount := 100 #or insta kill
+
+@export var killzone_size := Vector2(64,64)
+
+@onready var collision := $CollisionShape2D
+@onready var time := $Timer
+
 
 func _on_body_entered(body):
 	if body.is_in_group("players"):
