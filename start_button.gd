6d85b2d3d4e3442a5b190f5e8e5ef2dfc358fc86
maria <mariaguerries@gmail.com>
2026-01-13 12:03:38 -0500

    .

---
+++ start_button.gd
@@ -0,0 +1,9 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.pressed.connect(on_start_pressed)
+	
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://tileTest.tscn")
