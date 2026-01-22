7db2e0101cc2727fdaaaa9140d737457fe2d39cd
beni226 <benjamin.joseph8797@gmail.com>
2026-01-22 12:46:08 -0500

    enn

---
+++ nushroom_enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var speed = 70
+var player_chase = false
+var player = null
+
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position)/speed
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	#when player enter radious
+	print("working")
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
