c06e90429fc12008a5ec5d282f7f6c57808ee0b9
beni226 <benjamin.joseph8797@gmail.com>
2026-01-30 13:18:18 -0500

    init

---
+++ winZone.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+@export var kill_delay := 0.0   # optional delay before death
+
+func _on_body_entered(body):
+	if body.is_in_group("players"):
+		if kill_delay > 0:
+			await get_tree().create_timer(kill_delay).timeout
+		body.win()
