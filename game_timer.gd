6bc0f81ad644ae1aa127a5a1d6c6bcb8d31d3bf2
nahjakgrant-io <nahja.k.grant@gmail.com>
2026-02-04 08:37:46 -0500

    timer

---
+++ game_timer.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var label = $Label
+@onready var timer = $Timer
+
+func _ready():
+	timer.start()
+
+func time_left_to_play():
+	var time_left = timer.time_left
+	var minute = floor(time_left / 60)
+	var second = int(time_left) % 60
+	return [minute, second]
+
+func _process(delta):
+	label.text = "%02d:%02d" % time_left_to_play()
